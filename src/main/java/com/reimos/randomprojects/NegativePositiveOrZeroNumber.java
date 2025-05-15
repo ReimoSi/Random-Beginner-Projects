489b061a48d08c1652522e8c99848bb0a8460cfd
ReimoSi <sild.reimo@gmail.com>
2025-05-15 20:12:02 +0300

    NegativePositiveOrZeroNumber added

---
+++ src/main/java/com/reimos/randomprojects/NegativePositiveOrZeroNumber.java
@@ -0,0 +1,21 @@
+package com.reimos.randomprojects;
+
+public class NegativePositiveOrZeroNumber {
+    public static void main(String[] args) {
+
+        checkNumber(0);
+        checkNumber(4);
+        checkNumber(-3);
+    }
+
+    public static void checkNumber(int number) {
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
